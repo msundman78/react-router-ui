a61e8ef8a871bd702028d0459b3958dcc792a80e
Mathias Sundman <mathias.sundman@it-total.se>
2021-03-13 20:26:32 +0100

    Added NavBar

--- src/App.js
+++ src/App.js
@@ -1,28 +1,15 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import './App.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+//import './App.css';
 
-import Home from './Pages/Home';
-import About from './Pages/About';
-
-function Nav() {
-  return (
-    <div>
-      <div><Link to="/">Home</Link></div>
-      <div><Link to="/about">About</Link></div>
-    </div>
-  );
-} 
+import Nav from './Nav';
 
 function App() {
+
   return (
     <div className="App">
       <Router>
         <Nav />
-        <Switch>
-          <Route path="/about"><About /></Route>
-          <Route path="/"><Home /></Route>
-        </Switch>
       </Router>
     </div>
   );
