5c9c6955ca7d9de88f936016e8ab4973b685ae63
Mathias Sundman <mathias.sundman@it-total.se>
2021-03-13 16:41:49 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import './App.css';
+
+import Home from './Pages/Home';
+import About from './Pages/About';
+
+function Nav() {
+  return (
+    <div>
+      <div><Link to="/">Home</Link></div>
+      <div><Link to="/about">About</Link></div>
+    </div>
+  );
+} 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Nav />
+        <Switch>
+          <Route path="/about"><About /></Route>
+          <Route path="/"><Home /></Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
